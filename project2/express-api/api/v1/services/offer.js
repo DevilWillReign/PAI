050f40ba89fe2ba320ff7f91d9af6907fbd6c2a8
P S <ps0hakai.tenshi@gmail.com>
2022-07-14 14:50:08 +0200

    Improvements

--- project2/express-api/api/v1/services/offer.js
+++ project2/express-api/api/v1/services/offer.js
@@ -6,7 +6,7 @@ const addOffer = (body, callback) => {
     for (var k in body) {
         insert[":"+k] = body[k]
     }
-    db.query("SELECT end_date FROM itts WHERE itt_id=:itt_id)", {":itt_id": insert[":itt_id"]}, (rows) => {
+    db.query("SELECT end_date FROM itts WHERE itt_id=:itt_id", {":itt_id": insert[":itt_id"]}, (rows) => {
         if (Date.now() <= new Date(rows[0].end_date)) {
             db.query("INSERT INTO offers(institution_name,offer_value,offer_date,itt_id) VALUES (:institution_name,:offer_value,:offer_date,:itt_id)", insert, () => {
                 callback(true)
