998a3cb4e1c7c9a8c8c420f0684d01ed6c232b0d
P S <ps0hakai.tenshi@gmail.com>
2022-07-14 14:04:01 +0200

    Update Header.js

--- project2/front/src/components/common/Header.js
+++ project2/front/src/components/common/Header.js
@@ -3,10 +3,10 @@ import { NavLink } from "react-router-dom"
 const Header = () => {
     return (
         <nav>
-            <NavLink to="/">main page</NavLink>
-            <NavLink to="/itts">ITT list</NavLink>
-            <NavLink to="/itts/completed">Completed ITT list</NavLink>
-            <NavLink to="/itts/form">Add ITT</NavLink>
+            <NavLink to="/"><button>main page</button></NavLink>
+            <NavLink to="/itts"><button>ITT list</button></NavLink>
+            <NavLink to="/itts/completed"><button>Completed ITT list</button></NavLink>
+            <NavLink to="/itts/form"><button>Add ITT</button></NavLink>
         </nav>
     )
 }
